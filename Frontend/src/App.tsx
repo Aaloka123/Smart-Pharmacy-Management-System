a0f66851c62e557d8d4293e1e19518a200399bb3
Aaloka123 <aalokapoudel10@gmail.com>
2026-02-02 14:48:52 +0545

    feat: add routing with react-router-dom and create Home and Header components

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./UserPage/Home";
 
 const App = () => {
-  return <div className="bg-amber-600">hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* User pages */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
